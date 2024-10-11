c68f86b12a4ac13f015fdb3aba7f4d522484cc91
1216848438 <mcbutle4@asu.edu>
2024-10-11 16:14:10 -0500

    updates

--- script.js
+++ script.js
@@ -1,28 +1,12 @@
 "use strict";
 
-const questions = [
-  {
-    question: "What is the primary habitat of snow leopards?",
-    answers: ["Deserts", "Rainforests", "Mountains", "Grasslands"],
-    correct: 3
-  },
-  {
-    question: "How long is a snow leopard's tail?",
-    answers: ["60 cm", "100 cm", "120 cm", "150 cm"],
-    correct: 2
-  },
-  {
-    question: "What is a snow leopard's primary prey?",
-    answers: ["Deer", "Mountain goats", "Blue Sheep", "Birds"],
-    correct: 3
-  },
+let questions = [];
 
-  {
-    question: "What is the largest current threat to Snow Leopards?",
-    answers: ["Poaching", "Climate Change", "Habitat Loss", "Decline of Natural Prey"],
-    correct: 1 
-  }
-];
+async function loadQuestions() {
+  const response = await fetch('quiz.json');
+  questions = await response.json();
+  loadRandomQuestion();
+}
 
 function loadRandomQuestion() {
   const randomIndex = Math.floor(Math.random() * questions.length);
@@ -43,7 +27,7 @@ function loadRandomQuestion() {
   });
 }
 
-function checkAnswer(selectedIndex, correctIndex) {
+  async function checkAnswer(selectedIndex, correctIndex) {
   const answerItems = document.querySelectorAll('.answer-item');
   answerItems.forEach((item, index) => {
     if (index === correctIndex) {
@@ -52,20 +36,12 @@ function checkAnswer(selectedIndex, correctIndex) {
       item.classList.add('highlight-incorrect');
     }
   });
-}
 
-async function updatePoints(pointsToAdd) {
-  const user = auth.currentUser;
-  if(user) {
-    const userRef = db.collection('users').doc(user.uid);
-    const incremement = firebase.firestore.FieldValue.increment(pointsToAdd);
-    await userRef.update({points: increment});
-    const userDoc = await userRef.get();
-    document.getElementById('user-points').textContent = userDoc.data().points;
-  }
+  const userScore = selectedIndex === correctIndex ? 1 : 0;
+  await updatePoints(userScore);
+  await submitUserResult(userScore);
+  setTimeout(loadRandomQuestion, 2000);
 }
-
-window.onload = loadRandomQuestion;
 
 async function updatePoints(pointsToAdd) {
   const user = auth.currentUser;
@@ -77,6 +53,8 @@ async function updatePoints(pointsToAdd) {
     document.getElementById('user-points').textContent = userDoc.data().points;
   }
 }
+
+window.onload = loadQuestions;
 
 document.querySelectorAll('.sidebar a').forEach(link => {
   link.addEventListener('click', function(event) {
